6282e5969da0cc18bc8603baa7e7d378989b19ba
Jay Reddy <jayreddy@CC-LAPTOP-MICHAEL.local>
2023-07-28 12:17:15 -0700

    adds more infrastructure, creates basic go API gateway for grpc

--- go_client/main.go
+++ go_client/main.go
@@ -1,7 +1,18 @@
 package main
 
 import (
+	// "os"
 	"fmt"
+	"html"
+	"log"
 	"net/http"
-	"os"
 )
+
+func main() {
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+		// fmt.Printf("%q hit\n", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
