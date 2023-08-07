d14e2fabbec83850a0be9e4dc64225a7ca258e69
Jay Reddy <jayreddy@CC-LAPTOP-MICHAEL.local>
2023-08-07 09:29:23 -0700

    local package import problems

--- go_client/cmd/api_gateway/main.go
+++ go_client/cmd/api_gateway/main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
-	// "os"
-	"fmt"
-	"html"
+	"context"
 	"log"
 	"net/http"
+
+	pb "github.com/jayreddy040-510/spyder"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+	listenAddr := "localhost:8080"
+	gRPCAddress := "localhost:50051"
+
+	// Set up a connection to the gRPC server.
+	conn, err := grpc.Dial(gRPCAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("couldnt connect to gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	chatClient := pb.NewChatServiceClient(conn)
+
 	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
-		// fmt.Printf("%q hit\n", html.EscapeString(r.URL.Path))
+		message := r.URL.Query().Get("msg")
+		model := r.URL.Query().Get("model")
+
+		// Prepare gRPC request
+		req := &pb.ChatRequest{
+			Msg:   message,
+			Model: model,
+		}
+
+		// Call gRPC method
+		resp, err := chatClient.GetChat(context.Background(), req)
+		if err != nil {
+			http.Error(w, "error calling gRPC server: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Write response
+		w.Write([]byte(resp.Msg))
 	})
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Printf("Server listening at %v", listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
